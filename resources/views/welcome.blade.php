be773493c647c84b7e54d9bd2d763ca9687ee4c9
Naufal_abdullah <lnaufa90@gmail.com>
2025-12-26 05:56:58 +0700

    now what

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,16 +40,6 @@
                                 Watch video tutorials at
                                 <a href="https://laracasts.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                                     <span>Laracasts</span>
-                                    <svg
-                                        width="10"
-                                        height="11"
-                                    >
-                                        <path
-                                            d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
-                                            stroke="currentColor"
-                                            stroke-linecap="square"
-                                        />
-                                    </svg>
                                 </a>
                             </span>
                         </li>
